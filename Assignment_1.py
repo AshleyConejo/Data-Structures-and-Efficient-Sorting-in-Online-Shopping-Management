02b305cad17a5f7166be270d077df949480badef
Ashley <ashley.conejomolina@ontariotechu.net>
2024-07-03 16:17:01 -0400

    update 1

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,2 +1,11 @@
+product_file = open("product_data.txt","‘w+")
+
 class Product:
-    
+    def __init__(self, pd_ID, pd_Name, pd_Price, pd_Category):
+        self.pd_ID = pd_ID
+        self.pd_Name = pd_Name
+        self.pd_Name = pd_Price
+        self.pd_Category = pd_Category
+
+    def __repr__(self):
+        
